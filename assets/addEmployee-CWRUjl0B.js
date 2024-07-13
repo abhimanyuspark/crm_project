c659ffc8546708fe068572ce872e527ce15ddbd9
ZoroOneP <122513927+ZoroOneP@users.noreply.github.com>
2024-07-13 15:42:22 +0530

    Updates

--- assets/addEmployee-CWRUjl0B.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-D4AcEXSj.js";const o=()=>e.jsx("div",{children:"AddEmployee"});export{o as default};
---
+++ assets/editEmployee-C6KvWfMX.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-CPpqzfo9.js";const o=()=>e.jsx("div",{children:"EditEmployee"});export{o as default};
